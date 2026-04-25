f24ab0a8718f80a9ca6f683257563fe944732e42
Kevin001-spec <lflamme531@gmail.com>
2026-04-25 03:10:18 +0300

    fix: add missing Layout component to resolve Netlify build error

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import WhatsAppFAB from "@/components/WhatsAppFAB";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen bg-background flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        {children}
+      </main>
+      <WhatsAppFAB />
+    </div>
+  );
+};
+
+export default Layout;
